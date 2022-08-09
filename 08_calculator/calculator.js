8c358bd28c68e8a91b4b2c4f6ce368f86a28f0ab
Sebastian <ssansone124@gmail.com>
2022-08-09 17:50:00 -0400

    Completed exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((previousVal, currentVal) => previousVal+currentVal,0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((previousVal, currentVal) => previousVal*currentVal,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let currentVal=1;
+
+  for(let i=0;i<b;i++){
+    currentVal=currentVal*a;
+  };
+
+  return currentVal;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	currentVal=1;
+  
+  for(let i=1;i<a+1;i++){
+    currentVal=currentVal*i;
+  };
+  return currentVal; 
 };
 
 // Do not edit below this line
