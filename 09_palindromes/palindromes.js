df43100a4c2c56d1e4dcba1dcff779f19f803b4d
Sebastian <ssansone124@gmail.com>
2022-08-10 12:48:44 -0400

    Completed 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,36 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let myString=string.replace(/['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/g,"").replace(/\s/g, "").toLowerCase();
+    let myArray=myString.split("");
+    let revArray=[];
 
-};
+    for(let i=1;i<myArray.length+1;i++){
+        revArray.push(myArray[myArray.length-i]);
+    };
+    
+    console.log(myArray.length);
+    console.log(myString);
+    console.log(myArray);
+    console.log(revArray);
+    
+    let revString=revArray.join("");
+    revString.trim();
+    console.log(revString);
 
+    if(myString===revString){
+        return true;
+    } else {
+        return false;
+    }
+
+ 
+    /*  
+    for(let i=0;i<myArray.length;i++){
+        if(myArray[i]===revArray[i]){
+            return true;
+        } else {
+            return false;
+        };
+    };*/
+}; 
 // Do not edit below this line
 module.exports = palindromes;
