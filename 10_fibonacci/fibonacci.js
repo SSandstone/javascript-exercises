b5b1c8cb9e97174cf85fbc1e935740d5ae88f31c
Sebastian <ssansone124@gmail.com>
2022-08-10 15:06:56 -0400

    Completed 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    //new idea
+    //push numbers into array
+    //give it starting numbers to reference
+    let fibArr=[1,1];
 
+    //make strings allowable
+    let seqNum=parseInt(number);
+
+    //push last index + current index into array
+    for(let i=1;i<seqNum;i++){
+        fibArr.push(fibArr[i-1]+fibArr[i]);
+    }
+
+    //only works with positive integers
+    if(number>0){
+        return fibArr[seqNum-1];
+    } else {
+        return 'OOPS';
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// fibonacci(4)returns the 4th member of the series: 3  (1, 1, 2, 3)
+// 1 1 2 3 5 8 13 21...
+//+0 0 1 1 2 3 5 
